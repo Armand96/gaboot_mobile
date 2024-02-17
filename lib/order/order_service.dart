04d9f035811aa41d4a7338c17f9e4ad3bc1267c9
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-02-17 17:39:07 +0700

    add order services

---
+++ lib/order/order_service.dart
@@ -0,0 +1,24 @@
+import 'package:gaboot_mobile/order/order_model.dart';
+import 'package:gaboot_mobile/services/api.dart';
+import 'package:gaboot_mobile/services/response.dart';
+
+class CategoryService {
+  final String url = "order";
+
+  Future<ResponseAPI<Order>> create(Order param) async {
+    final response = await API().postAPI<Order>(url, param.toJson());
+    return response;
+  }
+
+  Future<ResponseAPI<Order>> getOrders() async {
+    final response = await API().getAPI<Order>(
+        url, (json) => Order.fromJson(json as Map<String, dynamic>));
+    return response;
+  }
+
+  Future<ResponseAPI<Order>> getOrder(int id) async {
+    final response = await API().getAPI<Order>(
+        '$url/$id', (json) => Order.fromJson(json as Map<String, dynamic>));
+    return response;
+  }
+}
