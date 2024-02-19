7653f6e6aa3fad987ed04d4536911fce744b5a51
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-02-19 11:30:15 +0700

    add payment service

---
+++ lib/payment/payment_service.dart
@@ -0,0 +1,12 @@
+import 'package:gaboot_mobile/payment/payment_model.dart';
+import 'package:gaboot_mobile/services/api.dart';
+import 'package:gaboot_mobile/services/response.dart';
+
+class CategoryService {
+  final String url = "payment";
+
+  Future<ResponseAPI<Payment>> create(Payment param) async {
+    final response = await API().postAPI<Payment>(url, param.toJson());
+    return response;
+  }
+}
